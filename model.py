dfbd999405fc40fe8f763bb22a797b1fbab042ab
Landon Smith <landjbs@gmail.com>
2019-11-11 11:02:57 -0500

    city locs tuple fix

--- model.py
+++ model.py
@@ -1,12 +1,20 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def gen_data(dLen, mainNodes):
-    assert dLen >= (mainNodes**2), 'dLen should be geq than main nodes squared'
-    dataMatrix = np.random.zeros(shape=(dLen, dLen))
-    cityLocs = np.random.randint(low=0, high=(dLen**2), size=mainNodes)
-    while len(set(cityLocs)) < mainNodes:
-        cityLocs.append(np.random.randint(0, (dLen**2), size=mainNodes))
+    assert (dLen**2) >= mainNodes, 'dLen squared should be geq than main nodes'
+    dataMatrix = np.zeros(shape=(dLen, dLen))
+    xLocs = np.random.randint(0, dLen, size=mainNodes)
+    yLocs = np.random.randint(0, dLen, size=mainNodes)
+    cityLocs = set(zip(xLocs, yLocs))
+    while len(cityLocs) < mainNodes:
+        x = np.random.int(0, dLen, size=mainNodes),
+        y = np.random.int(0, dLen, size=mainNodes)
+        if not (x, y) in cityLocs:
+            cityLocs.add((x, y))
     dataMatrix[cityLocs] = 10
-
     return dataMatrix
+
+plt.imshow(gen_data(10, 4))
+plt.show()
