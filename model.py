4b12e6c0a7629badefddf6c24f64e0700ac5609a
Landon Smith <landjbs@gmail.com>
2019-11-11 11:07:52 -0500

    taggging

--- model.py
+++ model.py
@@ -3,18 +3,23 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 def gen_data(dLen, mainNodes):
+    # assert and init data matrix
     assert (dLen**2) >= mainNodes, 'dLen squared should be geq than main nodes'
     dataMatrix = np.zeros(shape=(dLen, dLen))
     xLocs = np.random.randint(0, dLen, size=mainNodes)
     yLocs = np.random.randint(0, dLen, size=mainNodes)
-    cityLocs = set(zip(xLocs, yLocs))
-    while len(cityLocs) < mainNodes:
+    cityLocs = list(zip(xLocs, yLocs))
+    while len(set(cityLocs)) < mainNodes:
         x = np.random.int(0, dLen, size=mainNodes),
         y = np.random.int(0, dLen, size=mainNodes)
         if not (x, y) in cityLocs:
             cityLocs.add((x, y))
-    dataMatrix[cityLocs] = 10
+
+
+    finalX, finalY = zip(*cityLocs)
+    dataMatrix[finalX, finalY] = 10
     return dataMatrix
 
-plt.imshow(gen_data(10, 4))
-plt.show()
+for _ in range(5):
+    plt.imshow(gen_data(10, 4))
+    plt.show()
