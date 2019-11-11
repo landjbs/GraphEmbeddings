be8789e756932234987efce832576c129cb5cfe0
Landon Smith <landjbs@gmail.com>
2019-11-11 10:57:44 -0500

    dataMatrix locs log

--- model.py
+++ model.py
@@ -7,4 +7,6 @@ def gen_data(dLen, mainNodes):
     cityLocs = np.random.randint(low=0, high=(dLen**2), size=mainNodes)
     while len(set(cityLocs)) < mainNodes:
         cityLocs.append(np.random.randint(0, (dLen**2), size=mainNodes))
-    return np.
+    dataMatrix[cityLocs] = 10
+
+    return dataMatrix
