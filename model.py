2ce3d96a7f2c20fa08481f587b7a239f7a9631a7
Landon Smith <landjbs@gmail.com>
2019-11-11 10:53:55 -0500

    gen_data

--- model.py
+++ model.py
@@ -1,2 +1,8 @@
 import numpy as np
 import tensorflow as tf
+
+def gen_data(shape, mainNodes):
+    assert np.sqrt(shape) >= mainNodes, 'shape should be geq than main nodes'
+    dataMatrix = np.random.zeros(shape=(shape, shape))
+    cityLocs
+    return np.
