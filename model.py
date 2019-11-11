99be6cf9caef92db20f7d8d243899f384c82c74b
Landon Smith <landjbs@gmail.com>
2019-11-11 10:56:09 -0500

    cityLocs looper

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 import numpy as np
 import tensorflow as tf
 
-def gen_data(shape, mainNodes):
-    assert np.sqrt(shape) >= mainNodes, 'shape should be geq than main nodes'
-    dataMatrix = np.random.zeros(shape=(shape, shape))
-    cityLocs
+def gen_data(dLen, mainNodes):
+    assert dLen >= (mainNodes**2), 'dLen should be geq than main nodes squared'
+    dataMatrix = np.random.zeros(shape=(dLen, dLen))
+    cityLocs = np.random.randint(low=0, high=(dLen**2), size=mainNodes)
+    while len(set(cityLocs)) < mainNodes:
+        cityLocs.append(np.random.randint(0, (dLen**2), size=mainNodes))
     return np.
